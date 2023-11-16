88070c64cbe1598dd9b4a7883952d7052a5e2549
RafaelSilva27 <radasilva789@gmail.com>
2023-11-16 09:54:07 +0100

    COMMIT

--- Tarea 2 - Rafael Silva Alcántara/V1/script.js
+++ Tarea 2 - Rafael Silva Alcántara/V1/script.js
@@ -1,19 +1,22 @@
+//ZONA DE VARIABLES
 const listaEstudiantes = [
   { nombre: "Rafael", nota: [5, 8.5, 4.5, 9, 7, 7, 6, 6.5, 8, 4] }
 ];
 
 let notas = [];
 
+//ZONA DE EVENTOS
 document.addEventListener("DOMContentLoaded", () => {
   generarNotas();
 });
 
-function generarNotas() {
+//ZONA DE FUNCIONES
+function generarNotas() { //FUNCION PARA GENERAR LAS NOTAS EN EL HTML
   notas = listaEstudiantes.map(alumno => ({ nombre: alumno.nombre, nota: [...alumno.nota] }));
   mostrarNotas();
 }
 
-function mostrarNotas() {
+function mostrarNotas() { //FUNCION PARA MOSTRAR LAS NOTAS EN EL HTML
   const listaNotas = document.getElementById("listaNotas");
   listaNotas.innerHTML = "";
 
@@ -24,7 +27,7 @@ function mostrarNotas() {
   });
 }
 
-function calcularPromedio() {
+function calcularPromedio() { //FUNCION PARA CALCULAR EL PROMEDIO DE LAS NOTAS
   const promedioSpan = document.getElementById("promedioCalculo");
   const promedioResultado = document.getElementById("promedio");
 
@@ -33,7 +36,7 @@ function calcularPromedio() {
   promedioSpan.textContent = promedio.toFixed(2);
 }
 
-function calcularNotaMasAlta() {
+function calcularNotaMasAlta() { //FUNCION PARA CALCULAR LA NOTA MAS ALTA
   const notaMasAltaSpan = document.getElementById("NotaMasAlta");
   const notaMasAltaResultado = document.getElementById("NotaAlta");
 
@@ -41,7 +44,7 @@ function calcularNotaMasAlta() {
   notaMasAltaSpan.textContent = notaMasAlta;
 }
 
-function verificarSuspenso() {
+function verificarSuspenso() { //FUNCION QUE TE VERIFICA SI HAY ALGUN SUSPENSO
   const haySuspensoSpan = document.getElementById("haySuspenso");
   const suspensoResultado = document.getElementById("suspensoResultado");
 
